3e6eeb9e003831d9c4d31f56bab1259b803f9540
alxfed <alxfed@gmail.com>
2020-02-19 10:11:39 -0600

    initial commit

--- verify.py
+++ verify.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 """https://www.tensorflow.org/install/docker
 """
-import tensorflow as tf
+# import tensorflow as tf
 
 
 def main():
-    print(tf.reduce_sum(tf.random.normal([1000, 1000])))
+    # print(tf.reduce_sum(tf.random.normal([1000, 1000])))
     return
 
 
 if __name__ == '__main__':
     main()
-    print('\ndone')
