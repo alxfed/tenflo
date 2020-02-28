73cd820c2842648e1734cd53840900a24719a746
alxfed <alxfed@gmail.com>
2020-02-27 19:36:56 -0600

    data

--- build_input_pipelines.py
+++ build_input_pipelines.py
@@ -13,8 +13,7 @@ np.set_printoptions(precision=4)
 def main():
     '''
     To create an input pipeline, you must start with a data source.
-    For example, to construct a Dataset from data in memory,
-    you can use
+    For example, to construct a Dataset from data in memory, you can use:
     tf.data.Dataset.from_tensors() or
     tf.data.Dataset.from_tensor_slices().
     Alternatively, if your input data is stored in a file in the
