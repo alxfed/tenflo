02447d81e794d4d80a50899b205e12f018de28a1
alxfed <alxfed@gmail.com>
2020-02-27 19:47:05 -0600

    data

--- build_input_pipelines.py
+++ build_input_pipelines.py
@@ -6,6 +6,7 @@ import pathlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 np.set_printoptions(precision=4)
 
@@ -19,6 +20,12 @@ def main():
     Alternatively, if your input data is stored in a file in the
     recommended TFRecord format, you can use tf.data.TFRecordDataset().
     '''
+    a = 0
+    dataset = tf.data.Dataset.from_tensor_slices([8, 3, a, 8, 2, 1]) # from object(s) in memory
+    print(dataset)
+    # dataset is iterable
+    for elem in dataset:
+        print(elem.numpy())
     return
 
 
